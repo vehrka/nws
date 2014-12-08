46e8dfcd5a8e92ca3f82ef11ef1051c424032f7e
Pedro-Juan Ferrer <vehrka@gmail.com>
2014-12-08 23:16:41 +0100

    Changing the Configuration to a standard proc

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
+from config import config
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     app.config.from_envvar('SNWSETTINGS')
+    config[config_name].init_app(app)
 
     from .main import main_blueprint
     app.register_blueprint(main_blueprint)
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 from app import create_app
 from flask.ext.script import Manager, Shell
 
-app = create_app()
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
